98bc00dd2454ffb018d9f6ecc15a3a06918d5457
Luke Becker <lukeb667@proton.me>
2025-02-04 16:13:06 -0500

    Delete archinabox directory

--- archinabox/artifacts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('artifacts/', views.artifacts, name='artifacts'),
-    path('stlViewer/', views.stlViewer, name='stlViewer')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
